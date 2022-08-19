81d88670f5588140071349f5a2cd4e516eeec354
Jad Khalili <jad@videolab.ae>
2022-08-19 11:52:55 -0500

    Comments

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ let clicked = null;
 let clickable = true;
 
 // Constants
+// CHALLENGE: Randomly generate the colors!
 let COLORS = ["#f57d7d", "#f57d7d", "#f5b97d", "#f5b97d", "#fcfc77", "#fcfc77", "#7bfc77", "#7bfc77", "#779ffc",
 "#779ffc", "#a177fc", "#a177fc", "#e877fc", "#e877fc", "#fc77d6", "#fc77d6"]
 
 function load_tiles() {
     for(let i = 0; i < numTiles; ++i) {
+        // Create object to store jQuery element, boolean to track if matched, and color
         let tileObject = {};
         tileObject.tile = $("#" + i)
         tileObject.tile.css("background", TILE_COLOR)
@@ -21,13 +23,17 @@ function load_tiles() {
     }
     
     // CHALLENGE: Make it randomly assigned!
+    // Assign colors to tiles
     let tileAssignment = [6,11,3,2,15,7,0,8,5,12,10,1,9,13,14,4]
     for(let i = 0; i < tileAssignment.length; ++i) {
         tiles[tileAssignment[i]].color = COLORS[i]
     }
 
     for(let i = 0; i < tiles.length; ++i) {
+        // Event listener: If tile is clicked, do the following
         tiles[i].tile.click(function(){
+
+            // If game isn't over, the tiles are clickable, and the game isn't over
             if(!tiles[i].matched && clickable && !end) {
 
                 click_tile(tiles[i], "on") // Turn tile to "on" status
@@ -40,7 +46,7 @@ function load_tiles() {
                 // Otherwise, check if tiles match, check if game is over
                 else {
                     clickable = false; // Stop from clicking anything during transition time
-        
+                    
                     setTimeout(function(){
                         match_tiles(tiles[i], clicked);
                         clickable = true;
@@ -75,6 +81,7 @@ function match_tiles(tileObject1, tileObject2) {
 
     clicked = null; // Since both tiles clicked, turn off
 
+    // If the colors don't match, decrease attempts and change both colors back to tile color
     if(tileObject1.color != tileObject2.color) {
         numAttempts--;
         
@@ -85,6 +92,7 @@ function match_tiles(tileObject1, tileObject2) {
         return false;        
     }
     
+    // Otherwise, set matched boolean on both tiles to true
     tileObject1.matched = true;
     tileObject2.matched = true;
     matchesLeft--;
@@ -92,16 +100,19 @@ function match_tiles(tileObject1, tileObject2) {
 }
 
 function checkEnd() {
+    // If no matches left
     if(matchesLeft === 0) {
         end = true;
         endText.text("You won!")
     }
 
+    // If no attempts left
     else if(numAttempts === 0) {
         end = true;
         endText.text("You lost!")
     }
 }
 
+// Inital setup; load tiles and setup number of attempts shown to user
 load_tiles()
 attemptsText.text(numAttempts)
